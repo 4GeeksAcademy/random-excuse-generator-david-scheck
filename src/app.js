3dad02d8aef9ef266ec152f26c77226ddd62476c
scheck93 <109469032+scheck93@users.noreply.github.com>
2024-06-27 00:45:04 +0000

    Commit 1

--- src/app.js
+++ src/app.js
@@ -2,10 +2,41 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello World");
+  displayResult();
 };
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function randomAnswers() {
+  var randomAnswerWho = who[Math.floor(Math.random() * who.length)];
+  var randomAnswerAction = action[Math.floor(Math.random() * action.length)];
+  var randomAnswerWhat = what[Math.floor(Math.random() * what.length)];
+  var randomAnswerWhen = when[Math.floor(Math.random() * when.length)];
+
+  return (
+    randomAnswerWho +
+    " " +
+    randomAnswerAction +
+    " " +
+    randomAnswerWhat +
+    " " +
+    randomAnswerWhen
+  );
+}
+
+function displayResult() {
+  let excuse = randomAnswers();
+  document.getElementById("theExcuse").innerHTML = excuse;
+}
